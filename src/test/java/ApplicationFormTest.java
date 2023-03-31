c626d97e485f688d399c973776ef8aa9627e0f7d
VMantsevich <vladislava_mantsevich@mail.ru>
2023-04-01 00:05:45 +0300

    Corrected YML and SRC

--- src/test/java/ApplicationFormTest.java
+++ src/test/java/ApplicationFormTest.java
@@ -16,6 +16,14 @@ public class ApplicationFormTest {
     }
 
     @BeforeEach
+    public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+    @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
     }
