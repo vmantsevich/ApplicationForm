4783d945afb2307de2ca059f996a8978b76a617e
VMantsevich <vladislava_mantsevich@mail.ru>
2023-04-02 13:51:42 +0300

    SRC modified

--- src/test/java/ApplicationFormTest.java
+++ src/test/java/ApplicationFormTest.java
@@ -1,6 +1,4 @@
-
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
